b3fd4c24f3a1e01cd98ee8cefcc2c4bb40dbed81
anhengchangyua <188851312@qq.com>
2018-12-08 16:24:39 +0800

    use tabbar and tabbarview

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,11 @@ class App extends StatelessWidget {
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return DefaultTabController(
+      length: 3,
       child: Scaffold(
         appBar: AppBar(
+          backgroundColor: Colors.yellow,
           leading: IconButton(
             //为appBar左侧按钮
             icon: Icon(Icons.menu),
@@ -32,10 +34,29 @@ class Home extends StatelessWidget {
               onPressed: () => debugPrint('eee'),
             )
           ],
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(icon: Icon(Icons.screen_lock_portrait)),
+              Tab(icon: Icon(Icons.room_service)),
+              Tab(icon: Icon(Icons.date_range))
+            ],
+          ),
           title: Text('LT'),
           elevation: 5.0,
         ),
-        body: null,
+        body: TabBarView(
+          children: <Widget>[
+            Icon(Icons.access_alarm, size: 128.0),
+            Icon(
+              Icons.import_export,
+              size: 128.0,
+            ),
+            Icon(
+              Icons.image_aspect_ratio,
+              size: 128.0,
+            )
+          ],
+        ),
       ),
     );
   }
