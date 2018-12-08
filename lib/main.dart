e39f8942f333329fb3f3105227bbff39c9ef2052
anhengchangyua <188851312@qq.com>
2018-12-08 16:46:00 +0800

    on tap

--- lib/main.dart
+++ lib/main.dart
@@ -63,6 +63,19 @@ class Home extends StatelessWidget {
             )
           ],
         ),
+        bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            fixedColor: Colors.black,
+            items: [
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.home), title: Text('home')),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.history), title: Text('history')),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.list), title: Text('list')),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.person), title: Text('me'))
+            ]),
       ),
     );
   }
