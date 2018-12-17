f7bd1b965b3caaf24e292e96b9e1312b59433ed2
anhengchangyua <188851312@qq.com>
2018-12-17 15:47:08 +0800

    form add

--- lib/demo/form_demo.dart
+++ lib/demo/form_demo.dart
@@ -10,9 +10,90 @@ class FormDemo extends StatelessWidget {
           padding: EdgeInsets.all(16.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[TextFieldDemo()],
+            children: <Widget>[RegisterDemo()],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class RegisterDemo extends StatefulWidget {
+  @override
+  _RegisterDemoState createState() => _RegisterDemoState();
+}
+
+class _RegisterDemoState extends State<RegisterDemo> {
+  final registerFormKey = GlobalKey<FormState>();
+  String username, password;
+  bool autovalidate = false;
+  void handleSave() {
+    if (registerFormKey.currentState.validate()) {
+      registerFormKey.currentState.save();
+      debugPrint('$username');
+      debugPrint('$password');
+      Scaffold.of(context).showSnackBar(SnackBar(
+        content: Text('注册中...'),
+      ));
+    } else {
+      setState(() {
+        autovalidate = true;
+      });
+    }
+  }
+
+  String validateUsername(value) {
+    if (value.isEmpty) {
+      return '用户名不能为空';
+    }
+    return null;
+  }
+
+  String validatePassword(value) {
+    if (value.isEmpty) {
+      return '密码不能为空';
+    }
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: registerFormKey,
+      child: Column(
+        children: <Widget>[
+          TextFormField(
+            decoration: InputDecoration(labelText: '用户名', helperText: ''),
+            onSaved: (value) {
+              username = value;
+            },
+            autovalidate: autovalidate,
+            validator: validateUsername,
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: '密码', helperText: ''),
+            obscureText: true, //不展示密码
+            autovalidate: autovalidate,
+            onSaved: (value) {
+              password = value;
+            },
+            validator: validatePassword,
+          ),
+          SizedBox(
+            height: 32.0,
+          ),
+          Container(
+            width: double.infinity,
+            child: RaisedButton(
+              child: Text(
+                '注 册',
+                style: TextStyle(color: Colors.white, fontSize: 18.0),
+              ),
+              color: Colors.blueAccent,
+              onPressed: handleSave,
+            ),
+          )
+        ],
       ),
     );
   }
