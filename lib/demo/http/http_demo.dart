dfda541556202c409668f31b2744ddbee5d6a8a7
timeflies <188851312@qq.com>
2019-06-23 21:43:15 +0800

    HTTP DONE

--- lib/demo/http/http_demo.dart
+++ lib/demo/http/http_demo.dart
@@ -22,13 +22,14 @@ class _HttpDemoHomeState extends State<HttpDemoHome> {
   @override
   void initState() {
     super.initState();
-    fetchPost();
-    final post = {"title": "hellp", "bind": "BIND"};
-    print(post['title']);
-    var encode = json.encode(post);
-    print(encode);
-    var decode = json.decode(encode);
-    print(decode['title']);
+    fetchPosts().then((value) => print(value));
+    // final post = {"title": "hellp", "bind": "BIND"};
+    // print(post['title']);
+    // var encode = json.encode(post);
+    // print(encode);
+    // var decode = json.decode(encode);
+    // print(decode['title']);
+    // print(decode is Map);
   }
 
   @override
@@ -36,9 +37,37 @@ class _HttpDemoHomeState extends State<HttpDemoHome> {
     return Container();
   }
 
-  void fetchPost() async {
+  Future<List<Post>> fetchPosts() async {
     final response =
         await http.get('https://resources.ninghao.net/demo/posts.json');
-    print('${response.statusCode}');
+
+    if (response.statusCode == 200) {
+      var responseBody = json.decode(response.body);
+      List<Post> posts = responseBody['posts']
+          .map<Post>((item) => Post.fromJson(item))
+          .toList();
+      return posts;
+    } else {
+      throw Exception('fiald');
+    }
   }
+}
+
+class Post {
+  final int id;
+  final String title;
+  final String description;
+  final String author;
+  final String imgUrl;
+
+  Post(this.id, this.title, this.description, this.author, this.imgUrl);
+
+  Post.fromJson(Map json)
+      : id = json['id'],
+        title = json['title'],
+        description = json['description'],
+        author = json['author'],
+        imgUrl = json['imageUrl'];
+
+  Map toJson() => {'title': title, 'description': description};
 }
